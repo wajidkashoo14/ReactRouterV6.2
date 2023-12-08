e83266823d7d4e25df7e4245c816bc2bec505c59
wajid hussain kashoo <i.wajid.hussain@generalaeronautics.com>
2023-12-09 00:14:21 +0530

    added my github profile

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Layout from './components/Layout/Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact'
+import Github, { gitHubInfoLoader } from './components/Github/Github'
 
 
 const router = createBrowserRouter(
@@ -14,6 +15,10 @@ const router = createBrowserRouter(
       <Route path='' element={<Home />}/>
       <Route path='/about' element={<About />}/>
       <Route path='/contact' element={<Contact />}/>
+      <Route 
+      loader={gitHubInfoLoader}
+      path='/github' 
+      element={<Github />}/>
     </Route>
   )
 )
